d5695de16e517d11bc9d4bab68979432de22581b
mikayelyolchyan <mikayelyolchyan@gmail.com>
2025-07-31 21:26:16 +0400

    fixed race condition in take forks

--- philo/sources/take_put_forks.c
+++ philo/sources/take_put_forks.c
@@ -6,11 +6,20 @@
 /*   By: miyolchy <miyolchy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 17:01:07 by miyolchy          #+#    #+#             */
-/*   Updated: 2025/07/31 18:44:40 by miyolchy         ###   ########.fr       */
+/*   Updated: 2025/07/31 21:25:52 by miyolchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/headers/philo.h"
+
+bool	get_someone_died(t_data *data)
+{
+    bool val;
+    pthread_mutex_lock(&data->someone_died_mutex);
+    val = data->someone_died;
+    pthread_mutex_unlock(&data->someone_died_mutex);
+    return val;
+}
 
 void	philo_take_fork(t_philo *philo)
 {
@@ -19,20 +28,22 @@ void	philo_take_fork(t_philo *philo)
 		pthread_mutex_lock(philo->left_fork);
 		philo->left_fork_taken = true;
 		philo_print(philo, "has taken a fork");
-		if (philo->data->someone_died == true)
+		if (get_someone_died(philo->data))
 		{
 			pthread_mutex_unlock(philo->left_fork);
 			philo->left_fork_taken = false;
+			return ;
 		}
 		pthread_mutex_lock(philo->right_fork);
 		philo->right_fork_taken = true;
 		philo_print(philo, "has taken a fork");
-		if (philo->data->someone_died == true)
+		if (get_someone_died(philo->data))
 		{
 			pthread_mutex_unlock(philo->left_fork);
 			philo->left_fork_taken = false;
 			pthread_mutex_unlock(philo->right_fork);
 			philo->right_fork_taken = false;
+			return ;
 		}
 	}
 	else
@@ -40,19 +51,21 @@ void	philo_take_fork(t_philo *philo)
 		pthread_mutex_lock(philo->right_fork);
 		philo->right_fork_taken = true;
 		philo_print(philo, "has taken a fork");
-		if (philo->data->someone_died == true)
+		if (get_someone_died(philo->data))
 		{
 			pthread_mutex_unlock(philo->right_fork);
 			philo->right_fork_taken = false;
+			return ;
 		}
 		pthread_mutex_lock(philo->left_fork);
 		philo->left_fork_taken = true;
-		if (philo->data->someone_died == true)
+		if (get_someone_died(philo->data))
 		{
 			pthread_mutex_unlock(philo->left_fork);
 			philo->left_fork_taken = false;
 			pthread_mutex_unlock(philo->right_fork);
 			philo->right_fork_taken = false;
+			return ;
 		}
 		philo_print(philo, "has taken a fork");
 	}
